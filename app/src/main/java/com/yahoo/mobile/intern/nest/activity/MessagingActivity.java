8c06f8f05429818bd08559dd485a8488ef1a5a70
william <csak4718@gmail.com>
2015-08-24 17:19:56 +0800

    remove redundant code

--- app/src/main/java/com/yahoo/mobile/intern/nest/activity/MessagingActivity.java
+++ app/src/main/java/com/yahoo/mobile/intern/nest/activity/MessagingActivity.java
@@ -1,7 +1,6 @@
 package com.yahoo.mobile.intern.nest.activity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -29,15 +28,7 @@ import com.yahoo.mobile.intern.nest.event.RecipientEvent;
 import com.yahoo.mobile.intern.nest.utils.Common;
 import com.yahoo.mobile.intern.nest.utils.ParseUtils;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -207,31 +198,6 @@ public class MessagingActivity extends BaseActivity implements MessageClientList
     @Override
     public void onShouldSendPushData(MessageClient client, Message message, List<PushPair> pushPairs) {
         // Left blank intentionally
-        final String regId = new String(pushPairs.get(0).getPushData());
-
-        class SendPushTask extends AsyncTask<Void, Void, Void> {
-
-            @Override
-            protected Void doInBackground(Void... voids) {
-                HttpClient httpclient = new DefaultHttpClient();
-                HttpPost httppost = new HttpPost("http://your-domain.com?reg_id=" + regId);
-
-                try {
-                    HttpResponse response = httpclient.execute(httppost);
-                    ResponseHandler<String> handler = new BasicResponseHandler();
-                    Log.d("HttpResponse", handler.handleResponse(response));
-                } catch (ClientProtocolException e) {
-                    Log.d("ClientProtocolException", e.toString());
-                } catch (IOException e) {
-                    Log.d("IOException", e.toString());
-                }
-
-                return null;
-            }
-
-        }
-
-        (new SendPushTask()).execute();
     }
 
     @Override
