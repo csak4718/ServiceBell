c25835dd579997c2d88a2e98c5374e4ea3c2be01
FatMinMin <chiehmin18@gmail.com>
2015-08-25 14:10:10 +0800

    change color for seller layout

--- app/src/main/java/com/yahoo/mobile/intern/nest/adapter/CatchTaskDoneAdapter.java
+++ app/src/main/java/com/yahoo/mobile/intern/nest/adapter/CatchTaskDoneAdapter.java
@@ -10,9 +10,11 @@ import android.widget.TextView;
 import com.parse.CountCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.squareup.picasso.Picasso;
 import com.yahoo.mobile.intern.nest.R;
 import com.yahoo.mobile.intern.nest.utils.Common;
 import com.yahoo.mobile.intern.nest.utils.Utils;
@@ -22,6 +24,7 @@ import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by cmwang on 8/21/15.
@@ -36,7 +39,7 @@ public class CatchTaskDoneAdapter extends BaseAdapter {
 
         @Bind(R.id.txt_title) public TextView txtTitle;
         @Bind(R.id.txt_time) public TextView txtTime;
-        @Bind(R.id.txt_seller_name) public TextView txtSellerName;
+        @Bind(R.id.img_pic) public CircleImageView imgProfile;
 
         public ViewHolder(View view) {
             ButterKnife.bind(this, view);
@@ -69,7 +72,7 @@ public class CatchTaskDoneAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         final ViewHolder holder;
         if(convertView == null) {
-            convertView = mInflater.inflate(R.layout.card_my_done_task, parent, false);
+            convertView = mInflater.inflate(R.layout.card_catch_done_task, parent, false);
             holder = new ViewHolder(convertView);
             convertView.setTag(holder);
         }
@@ -80,21 +83,21 @@ public class CatchTaskDoneAdapter extends BaseAdapter {
         final ParseObject task = mList.get(position);
         holder.txtTitle.setText(task.getString(Common.OBJECT_QUESTION_TITLE));
         holder.txtTime.setText(task.getString(Common.OBJECT_QUESTION_TIME));
-        //setupSellerName(holder, task);
+
+        ParseUser buyer = task.getParseUser(Common.OBJECT_QUESTION_USER);
+        buyer.fetchInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject buyer, ParseException e) {
+                if (e == null) {
+                    ParseFile imgFile = buyer.getParseFile(Common.OBJECT_USER_PROFILE_PIC);
+                    Picasso.with(mContext)
+                            .load(imgFile.getUrl())
+                            .into(holder.imgProfile);
+                }
+            }
+        });
+
 
         return convertView;
-    }
-    private void setupSellerName(final ViewHolder holder, final ParseObject task) {
-        ParseUser seller = task.getParseUser(Common.OBJECT_QUESTION_DONE_USER);
-        if(seller != null) {
-            seller.fetchInBackground(new GetCallback<ParseObject>() {
-                @Override
-                public void done(ParseObject seller, ParseException e) {
-                    if (e == null) {
-                        holder.txtSellerName.setText(seller.getString(Common.OBJECT_USER_NICK));
-                    }
-                }
-            });
-        }
     }
 }
